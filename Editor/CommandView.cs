498ae6a74139f4cdf00abb7e268058e72efc1bd4
kanonji <kanonji@users.noreply.github.com>
2015-02-02 00:06:57 +0900

    re init on empty strategies

--- Editor/CommandView.cs
+++ Editor/CommandView.cs
@@ -10,18 +10,17 @@ public class CommandView : EditorWindow {
 	static List<ICommandStrategy> strategies = new List<ICommandStrategy>();
 
 	[MenuItem ("MyMenus/CommandView")]
-	public static void init () {
+	public static void Init () {
 		EditorWindow.GetWindow<CommandView> (false, "Command View");
 		if (0 == strategies.Count) {
-			strategies.Add (new Primitives ());
-			strategies.Add (new Lights ());
-			strategies.Add (new Sprites ());
-			strategies.Add (new Empty ());
-			strategies.Add (new Folder ());
+			InitCommand ();
 		}
 	}
 
 	public void OnGUI () {
+		if (0 == strategies.Count) {
+			InitCommand ();
+		}
 		command = EditorGUILayout.TextField (command);
 		if (Event.current.type == EventType.KeyUp && Event.current.keyCode == KeyCode.Return) {
 			strategies.ForEach ((ICommandStrategy strategy) => {
@@ -31,5 +30,14 @@ public class CommandView : EditorWindow {
 			});
 			command = "";
 		}
+	}
+
+	private static void InitCommand(){
+		strategies.Clear ();
+		strategies.Add (new Primitives ());
+		strategies.Add (new Lights ());
+		strategies.Add (new Sprites ());
+		strategies.Add (new Empty ());
+		strategies.Add (new Folder ());
 	}
 }
